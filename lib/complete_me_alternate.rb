cf1e5dd65938f26a9a7e40540df9bc8e1ab0883f
Dan Winter <dan.j.winter@gmail.com>
2015-10-28 19:45:59 -0600

    ranking hash at each node

---
+++ lib/complete_me_alternate.rb
@@ -0,0 +1,128 @@
+class CompleteMe
+  attr_accessor :root
+
+  def initialize
+    @root = Node.new("")
+  end
+
+  def insert(data)
+    @root.insert(data)
+  end
+
+  def count
+    @root.count
+  end
+
+  def suggest(prefix)
+    @root.suggest(prefix)
+  end
+
+  def select(prefix, word)
+    @root.select(prefix, word)
+  end
+
+  def populate(source)
+    arr = source.downcase.split("\n")
+    arr.each do |entry|
+      @root.insert(entry)
+    end
+    nil
+  end
+
+  def contain(prefix)
+    @root.contain(prefix)
+  end
+
+  def find_prefix(prefix)
+    @root.find_prefix(prefix)
+  end
+end
+
+class Node
+  attr_reader :links, :data
+  attr_accessor :word_indicator, :rank, :ranking_hash
+
+  def initialize(data)
+    @data = data
+    @links = {}
+    @word_indicator = false
+    @rank = 0
+    @ranking_hash = {}
+  end
+
+
+  def insert(value, counter=0)
+    ranking_hash[value] = 0
+    if links[value[0..counter]].nil?
+      links[value[0..counter]] = Node.new(value[0..counter])
+    end
+    if value[counter + 1].nil?
+      links[value[0..counter]].word_indicator = true
+    else
+      links[value[0..counter]].insert(value, counter + 1)
+    end
+  end
+
+  def count
+    if data == "" && links.empty?
+      0
+    elsif links.empty?
+      1
+    elsif word_indicator == true
+      1 + (links.values.map {|value| value.count}).reduce(&:+)
+    else
+      (links.values.map {|value| value.count}).reduce(&:+)
+    end
+  end
+
+  def contain(prefix)
+    new_arr = find_words_with_rank(prefix).map do |word_rank_pair|
+      word_rank_pair[0]
+    end
+    new_arr
+  end
+
+  def find_words_with_rank(prefix, counter=0, contains_array=[])
+    if word_indicator == true && data.include?(prefix)
+        contains_array << self.data
+    end
+      unless links.nil?
+        links.values.map {|value| value.find_words_with_rank(prefix, counter, contains_array)}
+      end
+    contains_array.uniq
+  end
+
+  def find_prefix(prefix, counter=0)
+    if data == prefix
+      return self
+    elsif data == prefix[0..counter]
+      links[prefix[0..counter + 1]].find_prefix(prefix, counter + 1)
+    else
+      unless links[prefix[0..counter]].nil?
+        links[prefix[0..counter]].find_prefix(prefix, counter)
+      end
+    end
+  end
+
+  def suggest(prefix)
+    start = find_prefix(prefix)
+    if start.ranking_hash.values.uniq == [0]
+      new_arr = start.ranking_hash.sort
+      final_arr = new_arr.map do |word_rank_pair|
+        word_rank_pair[0]
+      end
+      final_arr.sort!
+    else
+      new_arr = start.ranking_hash.sort {|a, b| a[1] <=> b[1] }.reverse
+      final_arr = new_arr.map do |word_rank_pair|
+        word_rank_pair[0]
+      end
+      final_arr
+    end
+  end
+
+  def select(prefix, word)
+    node = find_prefix(prefix)
+    node.ranking_hash[word] += 1
+  end
+end
---
+++ test/complete_me_test.rb
@@ -0,0 +1,108 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/complete_me_alternate'
+require 'pry'
+
+class CompleteMeTest < Minitest::Test
+
+  def setup
+    @complete = CompleteMe.new
+  end
+
+  def test_it_can_push_word
+    @complete.insert("word")
+    assert_equal "w", @complete.root.links["w"].data
+    assert_equal "wo", @complete.root.links["w"].links["wo"].data
+    assert_equal "wor", @complete.root.links["w"].links["wo"].links["wor"].data
+    assert_equal "word", @complete.root.links["w"].links["wo"].links["wor"].links["word"].data
+    assert_equal({}, @complete.root.links["w"].links["wo"].links["wor"].links["word"].links)
+  end
+
+  def test_it_can_push_multiple_words_that_do_not_share_letters
+    @complete.insert("ab")
+    @complete.insert("cd")
+
+    assert_equal "a", @complete.root.links["a"].data
+    assert_equal "ab", @complete.root.links["a"].links["ab"].data
+    assert_equal "c", @complete.root.links["c"].data
+    assert_equal "cd", @complete.root.links["c"].links["cd"].data
+  end
+
+  def test_it_can_insert_multiple_words_that_share_letters
+    @complete.insert("ab")
+    @complete.insert("ad")
+    assert_equal "a", @complete.root.links["a"].data
+    assert_equal "ab", @complete.root.links["a"].links["ab"].data
+    assert_equal "ad", @complete.root.links["a"].links["ad"].data
+  end
+
+  def test_it_can_flip_word_indicator
+    @complete.insert("ab")
+    @complete.insert("ad")
+    assert @complete.root.links["a"].links["ab"].word_indicator
+    assert @complete.root.links["a"].links["ad"].word_indicator
+    refute @complete.root.links["a"].word_indicator
+    @complete.insert("a")
+    assert @complete.root.links["a"].word_indicator
+  end
+
+  def test_it_can_count_zero_words
+    assert_equal 0, @complete.count
+  end
+
+  def test_it_can_count_one_word
+    @complete.insert("ab")
+    assert_equal 1, @complete.count
+  end
+
+  def test_it_can_count_multiple_words
+    @complete.insert("ab")
+    @complete.insert("ad")
+    assert_equal 2, @complete.count
+    @complete.insert("word")
+    @complete.insert("words")
+    @complete.insert("a")
+    assert_equal 5, @complete.count
+  end
+
+  def test_it_can_suggest_words_from_prefix
+    @complete.insert("word")
+    assert_equal ["word"], @complete.suggest("w")
+
+    @complete.insert("wombat")
+    assert_equal ["wombat", "word"], @complete.suggest("w")
+    @complete.insert("wally")
+    assert_equal ["wombat", "word"], @complete.suggest("wo")
+  end
+
+  def test_it_can_suggest_words_from_prefix_with_lots_of_other_words
+    @complete.insert("wink")
+    @complete.insert("word")
+    @complete.insert("wacky")
+    @complete.insert("wombat")
+    @complete.insert("wonky")
+    @complete.insert("wasted")
+    @complete.insert("winter")
+    @complete.insert("waste")
+    @complete.insert("wallow")
+    assert_equal ["wink", "winter"], @complete.suggest("wi")
+    assert_equal ["wombat", "wonky", "word"], @complete.suggest("wo")
+  end
+
+  def test_it_changes_rank_when_selected
+
+    @complete.insert("wacky")
+    @complete.insert("walrus")
+    assert_equal ["wacky", "walrus"], @complete.suggest("w")
+    @complete.select("w", "walrus")
+    assert_equal ["walrus", "wacky"], @complete.suggest("w")
+  end
+
+  def test_it_changes_rank_when_selected_multiple_times
+    @complete.insert("wacky")
+    @complete.insert("walrus")
+  end
+
+
+end
+
